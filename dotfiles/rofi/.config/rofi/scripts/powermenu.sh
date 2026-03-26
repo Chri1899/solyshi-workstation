64a4f7d9e8d347b7c32dcde1de8814fb156c16a4
Chri1899 <c.b-1@outlook.de>
2026-03-26 14:43:13 +0100

    feat: rofi finish off and powermenu changed

--- dotfiles/rofi/.config/rofi/scripts/powermenu.sh
+++ dotfiles/rofi/.config/rofi/scripts/powermenu.sh
@@ -1,33 +1,107 @@
 #!/bin/bash
 
-# Rofi 
-ROFI_CMD="rofi -dmenu -p Power -theme ~/.config/rofi/powermenu/powermenu.rasi"
+dir="$HOME/.config/rofi/powermenu/"
+theme='powermenu'
 
+# CMDs
+lastlogin="`last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7`"
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+# Options
+hibernate=''
 shutdown='󰐥'
 reboot='󰑓'
 lock=''
 suspend=''
 logout=''
-yes=''
-no=''
+yes='Y'
+no='N'
 
-# Menü
-options="$shutdown\n$reboot\n$suspend\n$logout"
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p " $USER@$host" \
+		-mesg " Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
 
-chosen="$(echo -e "$options" | $ROFI_CMD)"
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-mesg 'Are you Sure?' \
+		-theme ${dir}/${theme}.rasi
+}
 
-case "$chosen" in
-    "$shutdown")
-        ~/.config/rofi/scripts/power.sh shutdown
-        ;;
-    "$reboot")
-        ~/.config/rofi/scripts/power.sh reboot
-        ;;
-    "$suspend")
-        ~/.config/rofi/scripts/power.sh suspend
-        ;;
-    "$logout")
-        ~/.config/rofi/scripts/power.sh exit
-        ;;
+# Ask for confirmation
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
 
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		elif [[ $1 == '--hibernate' ]]; then
+			systemctl hibernate
+		elif [[ $1 == '--suspend' ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			fi
+		fi
+	else
+		exit 0
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $hibernate)
+		run_cmd --hibernate
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $suspend)
+		run_cmd --suspend
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
 esac
